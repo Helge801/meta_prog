08943289064c8b2c33d0642e3202ba8ed50fdb48
Bryan Lund <bryan.helge@gmail.com>
2018-02-16 11:42:50 -0700

    create methoods on the fly

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -1,34 +1,41 @@
-class String
-  def censor(bad_word)
-    self.gsub! "#{bad_word}", "CENSORED"
-  end
 
-  def num_of_chars
-    size
-  end
-end
+# ==================== Patching the method_missing method to make adaptable methodes ==================== #
 
-puts "the bunny was in trouble with the kings bunny".censor("bunny")
-puts "the bunny was in trouble with the kings bunny".num_of_chars
+# require 'ostruct'
 
+# class Author
+#   attr_accessor :first_name, :last_name, :genre
 
-# class Baseball
-# end
+#   def author
+#     OpenStruct.new(first_name: first_name, last_name: last_name, genre: genre)
+#   end
 
-# # p Baseball.new.swing
-
-# class Baseball
-#   def swing
-#     "Homerun"
+#   def method_missing(method_name, *arguments, &block)
+#     if method_name.to_s =~ /author_(.*)/
+#       author.send($1, *arguments, &block)
+#     else
+#       super
+#     end
 #   end
 # end
 
-# puts Baseball.new.swing
+# writer_guy = Author.new
+# writer_guy.first_name = 'Cal'
+# writer_guy.last_name = 'Newport'
+# writer_guy.genre = 'Computer Science'
 
-# class Baseball
-#   def swing
-#     "Strike"
+# puts writer_guy.author_first_name
+
+# ==================== two types of patching classes ==================== #
+# class String
+#   def censor(bad_word)
+#     self.gsub! "#{bad_word}", "CENSORED"
+#   end
+
+#   def num_of_chars
+#     size
 #   end
 # end
 
-# puts Baseball.new.swing
+# puts "the bunny was in trouble with the kings bunny".censor("bunny")
+# puts "the bunny was in trouble with the kings bunny".num_of_chars
